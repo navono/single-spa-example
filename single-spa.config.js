c974d06be6312f4d40021acbd44b353af02006dc
navono <navono007@gmail.com>
2019-03-13 15:10:23 +0800

    fix: 微调

--- single-spa.config.js
+++ single-spa.config.js
@@ -1,11 +1,33 @@
 
 import { registerApplication, start } from 'single-spa';
 
-// registerApplication('navBar', () => import('./src/navBar/navBar.app.js').then(mod => mod.navBar), () => true);
-registerApplication('home', () => import('./packages/home/home.app.js'), (location) => location.pathname === "" || location.pathname === "/" || location.pathname.startsWith('/home'));
-// registerApplication('angularJS', () => import('./src/angularJS/angularJS.app'), pathPrefix('/angularJS'));
+registerApplication(
+  // Name of our single-spa application
+  'home',
+  // Loading function
+  () => import('./packages/home/index.js'),
+  // Activity function
+  (location) => location.pathname === "" || 
+    location.pathname === "/" || 
+    location.pathname.startsWith('/home')
+);
+
+registerApplication('navBar', () => import('./packages/navBar/index.js').then(mod => mod.navBar), () => true);
+// registerApplication('navBar', () => import('./packages/navBar/navBar.app.js').then(mod => mod.navBar), () => true);
 
 
+// `loadingFunction` must be a function that returns a Promise (or an async function). 
+// The function will be called with no arguments when loading the application for the first time. 
+// The returned promise must resolve with the application code.
+
+// `activityFunction` must be a function that returns a truthy value that represents whether the application should be active, 
+// and must be a pure function. The function is provided window.location as the first argument. 
+// The most common scenario is to determine if an application is active by looking at window.location, 
+// but not always. In this case, home will be our root application so it will be shown at the root url paths as well as 
+// and url pathname that begins with /home.
+
+// call `start` in order for applications be mounted. Before start is called, applications will be loaded into the browser 
+// but not bootstrapped/mounted/unmounted.
 start();
 
 function pathPrefix(prefix) {
