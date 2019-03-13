874d8e2676ee26cee6959331460fd6583288bfb6
navono <navono007@gmail.com>
2019-03-13 19:25:59 +0800

    feat: add commitlint

---
+++ commitlint.config.js
@@ -0,0 +1,45 @@
+// module.exports = {
+// 	rules: {
+// 		'body-leading-blank': [1, 'always'],
+// 		'footer-leading-blank': [1, 'always'],
+// 		'header-max-length': [2, 'always', 72],
+// 		'scope-case': [2, 'always', 'lower-case'],
+// 		'subject-case': [
+// 			2,
+// 			'never',
+// 			['sentence-case', 'start-case', 'pascal-case', 'upper-case']
+// 		],
+// 		'subject-empty': [2, 'never'],
+// 		'subject-full-stop': [2, 'never', '.'],
+// 		'type-case': [2, 'always', 'lower-case'],
+// 		'type-empty': [2, 'never'],
+// 		'type-enum': [
+// 			2,
+// 			'always',
+// 			[
+// 				'build',
+// 				'chore',
+// 				'ci',
+// 				'docs',
+// 				'feat',
+// 				'fix',
+// 				'perf',
+// 				'refactor',
+// 				'revert',
+// 				'style',
+// 				'test'
+// 			]
+// 		]
+// 	}
+// };
+
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [2, 'always', [
+        "feat", "fix", "docs", "style", "refactor", "perf", "test", "build", "ci", "chore", "revert"
+    ]],
+    'subject-full-stop': [0, 'never'],
+    'subject-case': [0, 'never']
+  }
+};
